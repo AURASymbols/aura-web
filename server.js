dec221d4c4f67d7f43679ddb68ec1f5c9d05e1ef
AURA <gdjxvsbxjwgs@gmail.com>
2026-09-20 19:49:47 -0700

    style: load project brief prominence css

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ app.get(["/", "/index.html"], (req, res, next) => {
     const styles = [
       '<link rel="stylesheet" href="/layout-fix.css">',
       '<link rel="stylesheet" href="/hero-refinement.css">',
-      '<link rel="stylesheet" href="/proof-refinement.css">'
+      '<link rel="stylesheet" href="/proof-refinement.css">',
+      '<link rel="stylesheet" href="/platform-brief.css">'
     ];
     const styledPage = styles.reduce((current, style) => {
       const href = style.match(/href="([^"]+)"/)?.[1];
